506ba337fa8d2db6f313025b7db5b4c5dc95a6b1
Pedro Rabello <pedrommrabello@gmail.com>
2015-07-07 16:03:19 -0300

    FiltroDeFaturasTest criado

--- src/modulo2/exercicio/mocks/FiltroDeFaturasTest.java
+++ src/modulo2/exercicio/mocks/FiltroDeFaturasTest.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,49 +15,51 @@ public class FiltroDeFaturasTest {
 	
 	FiltroDeFaturas filtroDeFaturas;
 	RepositorioDeFaturas mock;
+	private ArrayList<Fatura> faturas;
 
 	@Before
 	public void setUp() throws Exception {		
 		mock = Mockito.mock(RepositorioDeFaturas.class);
 
 		filtroDeFaturas = new FiltroDeFaturas(mock);
+		faturas = new ArrayList<Fatura>();
+		Mockito.when(mock.todas()).thenReturn(faturas);
+
 	}
 
 	@Test
 	public void filtraMaioresQue2000() {
-		List<Fatura> faturas = new ArrayList<Fatura>();
 		faturas.add(new Fatura(Calendar.getInstance(), "João", 500));
 		faturas.add(new Fatura(Calendar.getInstance(), "Mané", 3000));
 		
-		Mockito.when(mock.todas()).thenReturn(faturas);
-		
 		List<Fatura> filtrada = filtroDeFaturas.filtra();
 		
-		Assert.assertEquals(1, filtrada.size());
-		
+		assertEquals(1, filtrada.size());
 	}
 	
 	@Test
-	public void filtraFaturaDaMicro$$oftMaiorQue2000(){
-		List<Fatura> faturas = new ArrayList<Fatura>();
-		faturas.add(new Fatura(Calendar.getInstance(), "MICROSOFT", 2000));
-		faturas.add(new Fatura(Calendar.getInstance(), "Mané", 300));
+	public void filtraFaturaAnteriorA1999ComValor2000(){
+		faturas.add(new Fatura(new GregorianCalendar(1998,Calendar.APRIL,1), "Xuxa", 2000));
+		faturas.add(new Fatura(Calendar.getInstance(), "zé", 1));
 		
-		Mockito.when(mock.todas()).thenReturn(faturas);
-				
 		List<Fatura> filtrada = filtroDeFaturas.filtra();
 		
-		Assert.assertEquals(1, filtrada.size());
+		assertFatura(filtrada, 2000);
 	}
 	
+	private void assertFatura(List<Fatura> lista, double... valores) {
+		
+		assertEquals(lista.size(), valores.length);
+		for(int i = 0; i < valores.length; i++) {
+			assertEquals(lista.get(0).getValor(), valores[i], 0.0001);
+		}
+	}
+
 	@Test
 	public void filtraFaturaSeForDaMicro$$oft(){
-		List<Fatura> faturas = new ArrayList<Fatura>();
 		faturas.add(new Fatura(Calendar.getInstance(), "MICROSOFT", 500));
 		faturas.add(new Fatura(Calendar.getInstance(), "Mané", 300));
 		
-		Mockito.when(mock.todas()).thenReturn(faturas);
-				
 		List<Fatura> filtrada = filtroDeFaturas.filtra();
 		
 		Assert.assertEquals(1, filtrada.size());
@@ -64,18 +67,16 @@ public class FiltroDeFaturasTest {
 	
 	@Test
 	public void filtraFaturasAnterioresA1999(){
-		List<Fatura> faturas = new ArrayList<Fatura>();
 		
 		Calendar dataAntiga = new GregorianCalendar(1998,Calendar.JULY,1);
 		
 		faturas.add(new Fatura(dataAntiga, "João", 500));
 		faturas.add(new Fatura(Calendar.getInstance(), "Mané", 200));
 		
-		Mockito.when(mock.todas()).thenReturn(faturas);
-				
 		List<Fatura> filtrada = filtroDeFaturas.filtra();
 		
 		Assert.assertEquals(1, filtrada.size());
+		Assert.assertEquals(500, filtrada.get(0).getValor(), 0.0001);
 	}
 
 }
